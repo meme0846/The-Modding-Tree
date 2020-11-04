17a8f9d25c298ebc8a4b88a89785d976fc3d2c6d
meme0846 <72827043+meme0846@users.noreply.github.com>
2020-11-04 14:42:50 -0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -26,3 +26,31 @@ addLayer("r", {
         ],
         layerShown(){return true},
 })
+addLayer("t", {
+        name: "tiers", // This is optional, only used in a few places, If absent it just uses the layer id.
+        symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
+        position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+        startData() { return {
+            unlocked: true,
+			points: new Decimal(0),
+        }},
+        color: "#4BDC13",
+        requires: Decimal.pow(this.points,2).add(2), // Can be a function that takes requirement increases into account
+        resource: "tiers", // Name of prestige currency
+        baseResource: "ranks", // Name of resource prestige is based on
+        baseAmount() {return player.r.points}, // Get the current amount of baseResource
+        type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+        exponent: 1, // Prestige currency exponent
+        gainMult() { // Calculate the multiplier for main currency from bonuses
+            mult = new Decimal(1)
+            return mult
+        },
+        gainExp() { // Calculate the exponent on main currency from bonuses
+            return new Decimal(1)
+        },
+        row: 1, // Row the layer is in on the tree (0 is the first row)
+        hotkeys: [
+            {key: "t", description: "Tier reset", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        ],
+        layerShown(){return true},
+})
