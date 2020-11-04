63ef4285ce5b146bffb93a690edd9b26960bd895
meme0846 <6d656d65b@gmail.com>
2020-11-03 21:04:25 -0800

    add tiers

--- js/layers.js
+++ js/layers.js
@@ -26,20 +26,20 @@ addLayer("r", {
         ],
         layerShown(){return true},
 })
-/*
-addLayer("p", {
-        name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-        symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+
+addLayer("t", {
+        name: "tiers", // This is optional, only used in a few places, If absent it just uses the layer id.
+        symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
         position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
         startData() { return {
-            unlocked: true,
+            unlocked: baseAmount().gt(2),
 			points: new Decimal(0),
         }},
         color: "#4BDC13",
-        requires: new Decimal(10), // Can be a function that takes requirement increases into account
-        resource: "prestige points", // Name of prestige currency
-        baseResource: "points", // Name of resource prestige is based on
-        baseAmount() {return player.points}, // Get the current amount of baseResource
+        requires: Decimal.pow(player.points,2).add(2), // Can be a function that takes requirement increases into account
+        resource: "tiers", // Name of prestige currency
+        baseResource: "ranks", // Name of resource prestige is based on
+        baseAmount() {return player.r.points}, // Get the current amount of baseResource
         type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
         exponent: 0.5, // Prestige currency exponent
         gainMult() { // Calculate the multiplier for main currency from bonuses
@@ -49,9 +49,9 @@ addLayer("p", {
         gainExp() { // Calculate the exponent on main currency from bonuses
             return new Decimal(1)
         },
-        row: 0, // Row the layer is in on the tree (0 is the first row)
+        row: 1, // Row the layer is in on the tree (0 is the first row)
         hotkeys: [
-            {key: "p", description: "Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+            {key: "t", description: "Tier reset", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
         ],
         layerShown(){return true},
-})*/
+})
