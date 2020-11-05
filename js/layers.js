b081e351111bb1de3a7a45591be7cb5769b4a927
meme0846 <72827043+meme0846@users.noreply.github.com>
2020-11-04 18:00:51 -0800

    bnnnnnnn

--- js/layers.js
+++ js/layers.js
@@ -35,7 +35,7 @@ addLayer("t", {
 			points: new Decimal(0),
         }},
         color: "#4BDC13",
-        requires: new Decimal(player[this.layer].points).pow(2).add(2),//testing fffff // Can be a function that takes requirement increases into account
+        requires: Decimal.times(this.points,this.points).add(3),//testing fffff // Can be a function that takes requirement increases into account
         resource: "tiers", // Name of prestige currency
         baseResource: "ranks", // Name of resource prestige is based on
         baseAmount() {return player.r.points}, // Get the current amount of baseResource
