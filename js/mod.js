55dde61a9e9e8f2f6485c1224d573afba3fab224
meme0846 <72827043+meme0846@users.noreply.github.com>
2020-11-06 11:44:34 -0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -34,8 +34,7 @@ function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
 
-	let gain = new Decimal(1)
-	gain=gain.mul(player.r.time)
+	let gain = new Decimal(player.r.velocity)
 	if (player.r.points.gte(2)) gain=gain.add(1)
 	if (player.r.points.gte(3)) gain=gain.times(Decimal.pow(1.1, player.r.points.sub(1)))
 	if (player.r.points.gte(4)) gain=gain.times(Decimal.pow(3, player.t.points))
