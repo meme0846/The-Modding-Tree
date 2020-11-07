29cefe61f2b16461bfbde6682c04a6165f581a7e
meme0846 <72827043+meme0846@users.noreply.github.com>
2020-11-06 18:03:32 -0800

    b

--- js/mod.js
+++ js/mod.js
@@ -45,12 +45,12 @@ function getPointGen() {
 		acceleration = acceleration.times(2)
 	}
 	if (player.r.points.gte(5)) {
-		acceleration=acceleration.times(Decimal.pow(3, player.r.points))
-		maxVelocity=maxVelocity.times(Decimal.pow(3, player.r.points))
+		acceleration=acceleration.times(Decimal.pow(3, player.r.points.sub(1)))
+		maxVelocity=maxVelocity.times(Decimal.pow(3, player.r.points.sub(1)))
 	}
 	if (player.r.points.gte(6)) {
-		acceleration=acceleration.times(Decimal.pow(1.975, player.r.points))
-		maxVelocity=maxVelocity.times(Decimal.pow(1.975, player.r.points))
+		acceleration=acceleration.times(Decimal.pow(1.975, player.r.points.sub(1)))
+		maxVelocity=maxVelocity.times(Decimal.pow(1.975, player.r.points.sub(1)))
 	}
 	if (player.t.points.gte(2) && player.r.points.gte(3)){
 		acceleration=acceleration.times(2)
