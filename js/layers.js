e54f3f066876f0105a57075f5c6fb614d08a985e
meme0846 <72827043+meme0846@users.noreply.github.com>
2020-11-07 13:29:43 -0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -130,3 +130,34 @@ addLayer("a", {
         ]
     }, 
 )
+addLayer("ro", {
+        name: "rockets", // This is optional, only used in a few places, If absent it just uses the layer id.
+        symbol: "RO", // This appears on the layer's node. Default is the id with the first letter capitalized
+        position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+        startData() { return {
+            unlocked: false,
+		points: new Decimal(1),
+        }},
+        color: "#4BDC13",
+        requires:5e8, // Can be a function that takes requirement increases into account
+        resource: "rockets", // Name of prestige currency
+	base: 1,
+        baseResource: "distance", // Name of resource prestige is based on
+        baseAmount() {return player.points}, // Get the current amount of baseResource
+        type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+        exponent: 1, // Prestige currency exponent
+        gainMult() { // Calculate the multiplier for main currency from bonuses
+            mult = new Decimal(1)
+
+            return mult
+        },
+        gainExp() { // Calculate the exponent on main currency from bonuses
+		exp=new Decimal(1)	
+            return exp
+        },
+        row: 2, // Row the layer is in on the tree (0 is the first row)
+        hotkeys: [
+            {key: "o", description: "Rocket Reset", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        ],
+        layerShown(){return (player.points.gte(5e8)||player.ro.points.gte(1))},
+})
